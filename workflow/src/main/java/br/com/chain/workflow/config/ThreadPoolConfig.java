550bf05c271aceaae74d502a81743ac14c160a4d
Danilo Silva <danilors.work@gmail.com>
2025-03-03 02:51:04 -0300

    config TomcatProtocolHandlerCustomizer

--- workflow/src/main/java/br/com/chain/workflow/config/ThreadPoolConfig.java
+++ workflow/src/main/java/br/com/chain/workflow/config/ThreadPoolConfig.java
@@ -1,14 +1,19 @@
 package br.com.chain.workflow.config;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 @Configuration
 public class ThreadPoolConfig {
+    private static final Logger logger = LoggerFactory.getLogger(ThreadPoolConfig.class.getSimpleName());
 
     @Bean("taskExecutor")
     public Executor taskExecutor() {
@@ -19,6 +24,14 @@ public class ThreadPoolConfig {
         executor.setThreadNamePrefix("Executor-");
         executor.initialize();
         return executor;
+    }
+
+    @Bean
+    TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
+        return protocolHandler -> {
+            logger.info("Configuring " + protocolHandler + " to use VirtualThreadPerTaskExecutor");
+            protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+        };
     }
 
 }
