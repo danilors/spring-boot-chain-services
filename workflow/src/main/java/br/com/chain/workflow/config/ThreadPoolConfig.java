a0f367994a0722c3d73ee020e94bcf61e404c89f
Danilo Silva <danilors.work@gmail.com>
2025-03-03 02:53:34 -0300

    add AsyncTaskExecutor

--- workflow/src/main/java/br/com/chain/workflow/config/ThreadPoolConfig.java
+++ workflow/src/main/java/br/com/chain/workflow/config/ThreadPoolConfig.java
@@ -6,11 +6,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.core.task.support.TaskExecutorAdapter;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
+@EnableAsync
 @Configuration
 public class ThreadPoolConfig {
     private static final Logger logger = LoggerFactory.getLogger(ThreadPoolConfig.class.getSimpleName());
@@ -24,6 +28,11 @@ public class ThreadPoolConfig {
         executor.setThreadNamePrefix("Executor-");
         executor.initialize();
         return executor;
+    }
+
+    @Bean
+    public AsyncTaskExecutor applicationTaskExecutor() {
+        return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
     }
 
     @Bean
