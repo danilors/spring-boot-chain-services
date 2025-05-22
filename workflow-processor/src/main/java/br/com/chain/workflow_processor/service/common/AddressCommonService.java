f7a3353d2f4dfcff0b0629f94aabbf01859380a4
Danilo Silva <danilors.work@gmail.com>
2025-05-21 21:11:04 -0300

    fix dockerfiles

--- workflow-processor/src/main/java/br/com/chain/workflow_processor/service/common/AddressCommonService.java
+++ workflow-processor/src/main/java/br/com/chain/workflow_processor/service/common/AddressCommonService.java
@@ -2,30 +2,39 @@ package br.com.chain.workflow_processor.service.common;
 
 import br.com.chain.workflow_processor.client.reactive.AddressReactiveClient;
 import br.com.chain.workflow_processor.enums.ServiceNamesEnum;
-import br.com.chain.workflow_processor.client.AddressClient;
 import br.com.chain.workflow_processor.model.Address;
 import br.com.chain.workflow_processor.model.Profile;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+
 @Slf4j
 @Service
 public class AddressCommonService implements CommonService {
 
 
-    private final AddressReactiveClient addressClient;
+    private final AddressReactiveClient addressReactiveClient;
 
-    public AddressCommonService(AddressReactiveClient addressClient) {
-        this.addressClient = addressClient;
+    public AddressCommonService(AddressReactiveClient addressReactiveClient) {
+        this.addressReactiveClient = addressReactiveClient;
     }
 
     public Mono<Address> getData(Profile profile) {
         log.info("getting address data");
         log.info("running in thread: {}", Thread.currentThread().getName());
-        return addressClient.getAddressById(profile.addressId());
+        return  getAddressByIdWithTimeout(profile.addressId());
     }
-
+    public Mono<Address> getAddressByIdWithTimeout(Integer id) {
+        return addressReactiveClient.getAddressById(id)
+                .timeout(Duration.ofSeconds(5)) // Apply a 5-second timeout
+                .onErrorResume(throwable -> {
+                    // Log the error and return a default Address
+                    System.err.println("Error occurred: " + throwable.getMessage());
+                    return Mono.just(Address.defaulAddres());
+                });
+    }
     @Override
     public ServiceNamesEnum getServiceName() {
         return ServiceNamesEnum.ADDRESS;
